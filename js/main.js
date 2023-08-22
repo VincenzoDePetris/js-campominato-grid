e6db079e80ccc6dcd0dfb182000e7015cdbc4fe0
VincenzoDePetris <vince02@live.it>
2023-08-22 08:22:48 +0200

    add: bonus

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 const button = document.getElementById("button");
 const griglia = document.getElementById("main");
+var select = document.getElementById("level");
 
 button.addEventListener("click", function () {
   generaGriglia();
 });
 
 function generaGriglia() {
-  for (let i = 1; i <= 100; i++) {
+  for (let i = 1; i <= select.value; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     cell.innerHTML = i;
@@ -17,5 +18,13 @@ function generaGriglia() {
     });
 
     griglia.append(cell);
+
+    if (select.value == 49) {
+      cell.classList.add("easy");
+    } else if (select.value == 81) {
+      cell.classList.add("medium");
+    } else {
+      cell.classList.add("hard");
+    }
   }
 }
